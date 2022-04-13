e609a0b2a76d3baaa46da7a8be8239ce2534eb8b
Allef Andre de Souza Moura <allefsouza@hotmail.com>
2022-04-12 22:47:33 -0300

    Exercicio-js

---
+++ moludo1/variaveis/index.js
@@ -0,0 +1,87 @@
+/* Interpretação 
+ Exercicio 1
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+console.log(a, b)
+Resposta = 10,5  
+ 
+
+ Exercicio 2
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c) 
+Reposta= 10,10,10
+
+ Exercicio 3
+let p = prompt("Quantas horas você trabalha por dia?")
+let t = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+Vai aparecer uma intereção com o usuario para ele colocar as inf. solicitadas em  let p e let t.
+o alert viria como o resultado da conta feita que seria a divisão t/p feita pela linha do alert.
+ FIM interpretação*/
+// Escrita codígo
+// Exercicio 1
+ /*let nome  
+ let idade 
+
+console.log (typeof nome );
+console.log (typeof idade );
+ foi impresso undefined por conta das variaveis nao conter nenhum valor.
+
+ nome = prompt("qual é seu nome?");
+ idade = prompt("qual é sua idade?");
+
+/*console.log(typeof nome);
+console.log(typeof idade);
+agora ambas estão constando como string, porque quando é usado prompt as inf. colocadas pelo usuario viram string.
+
+ console.log("Olá", nome, "voce tem", idade, "anos");
+*/
+//exercicio 2
+
+/*let blusa = prompt("Sua blusa é azul?")
+let estudando = prompt("você está estudando na labenu?")
+let gosto = prompt("está gostando?")
+
+console.log ("Sua blusa é azul?",blusa)
+console.log ("você está estudando na labenu?",estudando)
+console.log ("está gostando?", gosto)
+
+//exercicio 3
+
+let a = 10
+let b = 25
+
+c = a
+a = b
+b = c
+console.log (a, b)
+*/
+// Desafio Soma
+
+/*    
+let n1 = prompt("insira um numero")
+let n2 = prompt("insira outro numero")
+
+let n3 = Number (n1)+ Number (n2);
+
+alert(n3)
+*/
+
+// Desafio multiplicação
+
+/*let n1 = prompt("insira um numero")
+let n2 = prompt("insira outro numero")
+
+let n3 = n1*n2;
+
+alert(n3)
