c0f124c71d9a42b5b52abbdab2321a54c35a0ee8
Allef Andre de Souza Moura <allefsouza@hotmail.com>
2022-05-14 23:50:39 -0300

    finalizado

--- moludo1/revisao-js/exercicios.js
+++ moludo1/revisao-js/exercicios.js
@@ -80,47 +80,83 @@ function retornaObjetoEntreDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
+   let pares = []
+   let numeros = 0 
+   while (pares.length < n){
+      
+      pares.push(numeros)
+      numeros+=2;
+   }return pares
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
-
+   if(ladoA == ladoB && ladoA == ladoC && ladoB == ladoC){
+      return "Equilátero"
+   }else 
+      if (ladoA !== ladoB && ladoA !== ladoC && ladoB !== ladoC){
+      return "Escaleno"
+      }else 
+      return "Isósceles"
 }
 
 // EXERCÍCIO 10
 function retornaSegundoMaiorESegundoMenor(array) {
-  
+   array.sort((a, b) => a- b);
+   let segundoMaior = array[array.length-2]
+   let segundoMenor = array[1]
+   return [segundoMaior, segundoMenor]
 }
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+   let nomes = filme.atores.join(", ")
+   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${nomes}.`
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   
+      let anonimizada = {
+         ...pessoa,
+         nome:"ANÔNIMO"
+      }
+   return (anonimizada)
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+   return pessoas.filter(pessoa => {
+      return ((pessoa.idade > 14 && pessoa.idade < 60)
+          && pessoa.altura > 1.5)
+   })
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+  const pessoasNãoAutorizadas = pessoas.filter((pessoas, idex, array) =>{
+   return (pessoas.idade <= 14 || pessoas.idade > 60 || pessoas.altura < 1.5)
+      
+   })
+   return(pessoasNãoAutorizadas)
 }
+
 
 // EXERCÍCIO 14
 function retornaContasComSaldoAtualizado(contas) {
-
+  for ( let conta of contas){
+     while (conta.compras.length > 0){
+     let ultimaCompra = conta.compras.pop()
+     conta.saldoTotal = conta.saldoTotal - ultimaCompra 
+      
+   }
+ }return contas  
 }
-
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+   consultas.sort(function(a, b){
+     return a.nome < b.nome ? -1 : a.nome > b.nome ? 1 : 0;
+  })
+  return (consultas)
 }
 
 // EXERCÍCIO 15B
