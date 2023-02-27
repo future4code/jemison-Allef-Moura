93362e74f3cf8ac79c59078aa6c1660e1dddc9d0
Allef Souza <allefsouza@hotmail.com>
2023-02-27 20:46:11 -0300

    fluxo-de-dados

---
+++ modulo-5/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,19 @@
+import * as tipo from "./types"
+// exercicio 02
+export const products:tipo.Product[] =[
+    {
+        id:"1",
+        name:"sabão",
+        price:20
+    },
+    {
+        id:"2",
+        name:"carne",
+        price:30
+    } ,  
+    {
+        id:"3",
+        name:"vina",
+        price:15
+    }   
+    ] 
