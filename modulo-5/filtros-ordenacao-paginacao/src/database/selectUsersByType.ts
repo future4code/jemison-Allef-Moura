16b45366ab07f3966784373782f9f3b07f308f7c
Allef Souza <allefsouza@hotmail.com>
2023-03-28 19:34:19 -0300

    filtros-ordenacao-paginacao

---
+++ modulo-5/filtros-ordenacao-paginacao/src/database/selectUsersByType.ts
@@ -0,0 +1,8 @@
+import connection from "./connection";
+
+export default async function selectAllUsers(type: string): Promise<any> {
+    const result = await connection('aula48_exercicio')
+        .where({ type: type });
+
+    return result;
+};
